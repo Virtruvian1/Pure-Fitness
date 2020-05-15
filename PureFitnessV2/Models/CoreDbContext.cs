ec1edf135e72bca78a5539aeb2a7187aa8163704
Zachary Allen <zachary.thomas.allen@outlook.com>
2020-05-14 23:34:17 -0400

    Add CoreDbContext.cs; Connect to local database

---
+++ PureFitnessV2/Models/CoreDbContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace PureFitnessV2.Models
+{
+    public partial class CoreDbContext : DbContext
+    {
+        public CoreDbContext()
+        {
+        }
+
+        public CoreDbContext(DbContextOptions<CoreDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<UserProfile> UserProfile { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=PureFitnessDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserProfile>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.MiddleInitial).IsFixedLength();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ PureFitnessV2/Models/UserProfile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PureFitnessV2.Models
+{
+    public partial class UserProfile
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [StringLength(1)]
+        public string MiddleInitial { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+        [StringLength(50)]
+        public string NickName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string UserName { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string EmailAddress { get; set; }
+        [Required]
+        public string AccreditedProfile { get; set; }
+    }
+}
